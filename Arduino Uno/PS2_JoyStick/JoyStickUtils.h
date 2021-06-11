d166df5f6688715ca264759d62af5859775446dd
Rickson2019 <JudaoZhong@gmail.com>
2021-06-10 19:40:45 -0700

    updated ps2 joystick functions for the servo project

---
+++ Arduino Uno/PS2_JoyStick/JoyStickUtils.h
@@ -0,0 +1,43 @@
+#include "PS2X_lib.h"  //for v1.6
+
+PS2X ps2x; // create PS2 Controller Class
+
+int error = ps2x.config_gamepad(10,12,11,13, false, false);   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+byte type = 0;
+byte vibrate = 0;
+
+
+void joystick_setup(){
+
+ if(error == 0){
+    Serial.println("Found Controller, configured successful");
+    Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
+    Serial.println("holding L1 or R1 will print out the analog stick values.");
+    Serial.println("Go to www.billporter.info for updates and to report bugs.");
+  }
+   
+  else if(error == 1)
+    Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
+   
+  else if(error == 2)
+    Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
+   
+  else if(error == 3)
+    Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+   
+  type = ps2x.readType(); 
+    
+  switch(type) {
+    case 0:
+      Serial.println("Unknown Controller type");
+      break;
+    case 1:
+      Serial.println("DualShock Controller Found");
+      break;
+    case 2:
+      Serial.println("GuitarHero Controller Found");
+      break;
+    default:
+      break;
+  }
+}
