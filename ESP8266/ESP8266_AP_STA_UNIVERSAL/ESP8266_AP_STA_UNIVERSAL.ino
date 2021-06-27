9e7870207dd2b271e43c480f1d7a5d99d35a6ce9
Rickson2019 <JudaoZhong@gmail.com>
2021-06-26 19:41:18 -0700

    modified sketch to implement deepsleep features

--- ESP8266/ESP8266_AP_STA_UNIVERSAL/ESP8266_AP_STA_UNIVERSAL.ino
+++ ESP8266/ESP8266_AP_STA_UNIVERSAL/ESP8266_AP_STA_UNIVERSAL.ino
@@ -9,7 +9,7 @@
   copies or substantial portions of the Software.
 */
 
-// Modified by JudaoZhong
+// Modified by Judao Zhong JudaoZhong@gmail.com
 
 // Import required libraries
 #include <ESP8266WiFi.h>
@@ -17,60 +17,31 @@
 #include <ESPAsyncWebServer.h>
 #include <FS.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
 #include <ArduinoJson.h>
 
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+#include "utils/deepsleep_utils.h"
 
-// Replace with your network credentials
+// -------------------- Network Settings --------------------
+// Replace with credentials for your WiFi
+// that is connected to the Internet(for STA mode)
+// 接入互联网的WiFi名和密码(STA mode)
 char *sta_ssid = "NO.1";
 char *sta_password = "Home84039746";
 
+// Replace with credentials for the WiFi AccessPoint
+// that is running on your ESP8266
+// 8266本机的AP名和密码(AP mode)
 char* ap_ssid = "ESP8266_Weather_Station";
 char* ap_password = "12345678";
 
-
-// Set LED GPIO
-const int ledPin = 2;
-// Stores LED state
-String ledState;
-
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-
-String getTemperature()
-{
-  float temperature = bme.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  //float temperature = 1.8 * bme.readTemperature() + 32;
-  Serial.println(temperature);
-  return String(temperature);
-}
-
-String getHumidity()
-{
-  float humidity = bme.readHumidity();
-  Serial.println(humidity);
-  return String(humidity);
-}
-
-String getPressure()
-{
-  float pressure = bme.readPressure() / 100.0F;
-  Serial.println(pressure);
-  return String(pressure);
-}
-
 // 获取原Wifi名字
+// sta_ssid getter()
 String get_sta_ssid() {
   return sta_ssid;
 }
 
 // 获取原密码
+// sta_password getter()
 String get_sta_password()
 {
   return sta_ssid;
@@ -84,6 +55,38 @@ void set_sta_ssid_password(String new_sta_ssid, String new_sta_password)
   sta_ssid = &new_sta_ssid[0];
   sta_password = &new_sta_password[0];
 }
+// ____________________________________________________________
+
+
+// ------------------ Power Saving Sleep Settings ---------------
+// for deepsleep config
+int deepsleep_minutes = 0;
+int deepsleep_seconds = 0;
+
+//
+int get_deepsleep_minutes() {
+  return deepsleep_minutes;
+}
+
+//
+int get_deepsleep_seconds() {
+  return deepsleep_seconds;
+}
+
+//
+void set_deepsleep_minutes(int ds_minutes) {
+  deepsleep_minutes = ds_minutes;
+}
+
+//
+void set_deepsleep_seconds(int ds_seconds) {
+  deepsleep_seconds = ds_seconds;
+}
+// ______________________________________________________________
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
 
 // Replaces placeholder with LED state value
 String processor(const String &var)
@@ -121,15 +124,6 @@ void setup()
   WiFi.mode(WIFI_AP_STA);
   // Serial port for debugging purposes
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-
-  //  // Initialize the sensor
-  //  if (!bme.begin(0x76))
-  //  {
-  //    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-  //    while (1)
-  //      ;
-  //  }
 
   // Initialize SPIFFS
   if (!SPIFFS.begin())
@@ -138,15 +132,14 @@ void setup()
     return;
   }
 
-
-  //  // Begin Access Point
-  //  WiFi.softAP(ap_ssid, ap_password);
-  //  Serial.println("softAP IP:");
-  //  Serial.print(WiFi.softAPIP());
-  //  Serial.println("AP_SSID");
-  //  Serial.println(ap_ssid);
-  //  Serial.println("AP_PASSWORD");
-  //  Serial.println(ap_password);
+  // Begin Access Point
+  WiFi.softAP(ap_ssid, ap_password);
+  Serial.println("softAP IP:");
+  Serial.print(WiFi.softAPIP());
+  Serial.println("AP_SSID");
+  Serial.println(ap_ssid);
+  Serial.println("AP_PASSWORD");
+  Serial.println(ap_password);
 
 
   // Connect to Wi-Fi
@@ -167,7 +160,6 @@ void setup()
   {
     //    request->send(SPIFFS, "/index.html", String(), false, processor);
     request->send(SPIFFS, "/index.html", "text/html");
-
   });
 
   // Route to load index.css file
@@ -191,50 +183,30 @@ void setup()
   });
 
   // 设置页 的javascript文件 Route to config.js
-  server.on("/config.css", HTTP_GET, [](AsyncWebServerRequest * request)
+  server.on("/config.js", HTTP_GET, [](AsyncWebServerRequest * request)
   {
     request->send(SPIFFS, "/config.js", "script/javascript");
   });
+
+  //_____________________________________________________________________________
 
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Webpage Files ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
   // =========================== GPIO Manipulation ==============================
 
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest * request)
-  {
-    digitalWrite(ledPin, HIGH);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest * request)
-  {
-    digitalWrite(ledPin, LOW);
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
 
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^ GPIO Manipulation ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
   // =========================== DATA READ ENDPOINTS ==============================
 
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest * request)
-  {
-    request->send_P(200, "text/plain", getTemperature().c_str());
-  });
-
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest * request)
-  {
-    request->send_P(200, "text/plain", getHumidity().c_str());
-  });
-
-  server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest * request)
-  {
-    request->send_P(200, "text/plain", getPressure().c_str());
-  });
-
+//  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest * request)
+//  {
+//    request->send_P(200, "text/plain", getTemperature().c_str());
+//  });
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^ DATA READ ENDPOINTS ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+
+  // =========================== SETUPs ===========================================
   // 获取WiFi的SSID
   // Send WiFi SSID to the front end
   server.on("/sta_ssid", HTTP_GET, [](AsyncWebServerRequest * request)
@@ -271,25 +243,13 @@ void setup()
 
       strcpy(sta_ssid , sta_ssid_new);
       strcpy(sta_password , sta_password_new);
-      
+
       Serial.println(sta_ssid_new);
       Serial.println(sta_password_new);
       //      request->send(200, "text/plain", request->getParam("body", true)->value());
 
 
       WiFi.disconnect();
-      ////      WiFi.mode(WIFI_OFF);
-      //      delay(4000);
-      //
-      //      Serial.println("disconencted");
-      ////      WiFi.mode(WIFI_AP_STA);
-      ////
-      //
-      //      // Print ESP32 Local IP Address
-      //      Serial.println(WiFi.localIP());
-      //      // Start server
-      //      server.begin();
-
     }
     else
     {
@@ -298,12 +258,40 @@ void setup()
     }
   });
 
+  // Change Deepsleep Status
+  // 睡眠模式的时长设置
+  server.on("/update_deepsleep", HTTP_POST, [](AsyncWebServerRequest * request) {
+    Serial.println("updating deepsleep config");
+    if (request->hasParam("body", true))
+    {
+      // This is important, otherwise the sketch will crash if there is no body
+      String json_body = String(request->getParam("body", true)->value());
+      Serial.println(json_body);
+
+      DynamicJsonDocument doc(1024);
+      deserializeJson(doc, &json_body[0]);
+
+      const char* deepsleep_minutes_char_arr = doc["deepsleep_minutes"];
+      const char* deepsleep_seconds_char_arr = doc["deepsleep_seconds"];
+
+      deepsleep_minutes = atoi(deepsleep_minutes_char_arr);
+      deepsleep_seconds = atoi(deepsleep_seconds_char_arr);
+    }
+  });
+
+
+
+  // =========================== SETUPs ===========================================
+
+
   // Start server
   server.begin();
 }
 
 void loop()
 {
+  // 检查是否连接，如果断开就重连
+  // Check status and reconnect
   if (WiFi.status() !=  WL_CONNECTED) {
     WiFi.begin(sta_ssid, sta_password);
 
@@ -312,11 +300,22 @@ void loop()
       delay(1000);
       Serial.println("Connecting to WiFi..");
     }
-    
+
     // Print ESP32 Local IP Address
     Serial.println(WiFi.localIP());
     // Start server
     server.begin();
-
   }
+
+  // 如果设置了deepsleep 那么就进入睡眠模式
+  // If deepsleep is required
+  // then we put it into the sleep mode
+  if ((get_deepsleep_minutes() != 0)
+      && ( get_deepsleep_seconds() != 0 )
+     )
+  {
+    deepsleep_for_minutes(get_deepsleep_minutes());
+    deepsleep_for_seconds(get_deepsleep_seconds());
+  }
+
 }
---
+++ ESP8266/ESP8266_AP_STA_UNIVERSAL/utils/deepsleep_utils.h
@@ -0,0 +1,31 @@
+void deepsleep_for_seconds(int seconds)
+{
+    int sleep_micro_seconds = seconds * 1000 * 1000;
+    Serial.println("I'm going into deep sleep mode for" + String(seconds) + " seconds");
+    ESP.deepSleep(sleep_micro_seconds);
+}
+
+void deepsleep_for_minutes(int minutes)
+{
+    // 60 sec 10e6 us
+    int sleep_micro_seconds = minutes * 60 * 1000 * 1000;
+    Serial.println("I'm going into deep sleep mode for" + String(minutes) + " minutes");
+    ESP.deepSleep(sleep_micro_seconds);
+}
+
+void deepsleep_for_hours(int hours)
+{
+    // 60min 60 sec 10e6 us
+    int sleep_micro_seconds = hours * 60 * 60 * 1000 * 1000;
+    Serial.println("I'm going into deep sleep mode for" + String(hours) + " hours");
+    ESP.deepSleep(sleep_micro_seconds);
+}
+
+void serial_setup()
+{
+    Serial.begin(9600);
+    Serial.setTimeout(2000);
+    while (!Serial)
+    {
+    }
+}
