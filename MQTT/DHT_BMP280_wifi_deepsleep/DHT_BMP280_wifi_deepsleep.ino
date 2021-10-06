e2e06472374c0237f7c449234893a96fd72df71e
Rickson2019 <JudaoZhong@gmail.com>
2021-10-07 01:18:03 +0800

    Updated MQTT controlled room monitor

--- MQTT/DHT_BMP280_wifi_deepsleep/DHT_BMP280_wifi_deepsleep.ino
+++ MQTT/DHT_BMP280_wifi_deepsleep/DHT_BMP280_wifi_deepsleep.ino
@@ -1,15 +1,30 @@
 
-// MQTT Broker
+// ======= MQTT Broker settings ========
 const char *mqtt_broker = "192.168.2.2";
 char *topic = "wemos-room-monitor-02";
 const char *mqtt_username = "";
 const char *mqtt_password = "";
 const int mqtt_port = 18080;
+// -------------------------------------
+
+// Deep Sleep Related
+enum SLEEP_MODE
+{
+  SLEEP_FOR_HOURS,
+  SLEEP_FOR_MINUTES,
+  SLEEP_FOR_SECONDS
+};
+
+SLEEP_MODE deep_sleep_mode = SLEEP_FOR_SECONDS;
+
+int sleep_duration = 0;
+// --------------------
 
 #include <ArduinoJson.h>
-#include "utils/wifi_utils_v2.h"
+#include "utils/wifi_utils_v2.h" // configure wifi ssid and password here
 #include "utils/deep_sleep_utils.h"
-#include "utils/mqtt_utils_v2.h"
+#include "utils/mqtt_utils_v2.h" // - configure broker settings here.
+// - edit your call back function here
 #include "utils/bmp280_utils_v2.h"
 #include "utils/dht_utils_v2.h"
 
@@ -24,20 +39,31 @@ void setup()
   connect_to_broker();
 }
 
+/**
+   https://stackoverflow.com/questions/67639065/mqtt-client-not-subscribing-to-a-given-topic-or-callback-not-working-as-intende
+   You need a client.loop(); line in your loop() main function. Without that line, you MQTT code never gets executed.
+*/
 void loop()
 {
+  client.loop(); // Something you must have.
+  //  mqtt_update(topic, &read_BMP()[0]);
+  //  mqtt_update(topic, &DHT_read()[0]);
 
-  mqtt_update(topic, &read_BMP()[0]);
-  mqtt_update(topic, &DHT_read()[0]);
-
-  delay(3000); //间歇3秒更新
-//  deep_sleep_for_seconds(5);
+  //  delay(10000); //间歇3秒更新
+  //  Serial.println("10 sec passed");
+  //  deep_sleep_for_seconds(5);
+  if (sleep_duration != 0)
+  {
+    switch (deep_sleep_mode)
+    {
+    case SLEEP_FOR_HOURS:
+    {
+      deep_sleep_for_hours(sleep_duration);
+    }
+    case SLEEP_FOR_MINUTES:
+    {
+      deep_sleep_for_minutes
+    }
+    }
+  }
 }
-
-//void merge(JsonObject dest, JsonObjectConst src)
-//{
-//   for (auto kvp : src)
-//   {
-//     dest[kvp.key()] = kvp.value();
-//   }
-//}
--- MQTT/DHT_BMP280_wifi_deepsleep/utils/mqtt_utils_v2.h
+++ MQTT/DHT_BMP280_wifi_deepsleep/utils/mqtt_utils_v2.h
@@ -1,8 +1,71 @@
 #include <PubSubClient.h>
-
 //"wifiClient" defined in wifi_utils_v2.h
 PubSubClient client(wifiClient);
+
+// FIXME: not working as expected, find out why.
+void handle_change_deep_sleep_time(char *payload)
+{
+    Serial.println("handle_change_deep_sleep_time");
+
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+
+    if (error)
+    {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+    }
+
+    double deep_sleep_mode = doc["deep_sleep_mode"];
+    double sleep_duration = doc["sleep_duration"];
+
+    Serial.println(payload);
+    Serial.println(deep_sleep_mode);
+    Serial.println(sleep_duration);
+}
+
 void callback_function(char *topic, byte *payload, unsigned int length)
+{
+
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    String received_msg = "";
+
+    StaticJsonDocument<200> doc;
+
+    for (int i = 0; i < length; i++)
+    {
+        // Serial.print((char)payload[i]);
+        received_msg += (char)payload[i];
+    }
+
+    Serial.println(received_msg);
+    DeserializationError error = deserializeJson(doc, received_msg);
+
+    if (error)
+    {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+    }
+    const char *setting = doc["setting"];
+
+    Serial.println(setting);
+
+    if (setting == "deep_sleep")
+    {
+        Serial.println("deep_sleep");
+        handle_change_deep_sleep_time(&received_msg[0]);
+    }
+
+    Serial.println();
+    Serial.println("--------------- Done -----------------------");
+}
+
+//
+void check_same_topic(char *topic, byte *payload, unsigned int length)
 {
     Serial.print("Message arrived in topic: ");
     Serial.println(topic);
@@ -20,9 +83,10 @@ void connect_to_broker()
 {
     client.setServer(mqtt_broker, mqtt_port);
     client.setCallback(callback_function);
+
     while (!client.connected())
     {
-        String client_id = "esp8266-client-";
+        String client_id = "wemos-room-monitor-02";
         client_id += String(WiFi.macAddress());
         Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
         if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
@@ -48,3 +112,11 @@ void mqtt_update(char *topic_endpoint, char *sensor_data_json)
     // publish to the mqtt server
     client.publish(topic_endpoint, &sensor_data_json[0]);
 }
+
+//void merge(JsonObject dest, JsonObjectConst src)
+//{
+//   for (auto kvp : src)
+//   {
+//     dest[kvp.key()] = kvp.value();
+//   }
+//}
